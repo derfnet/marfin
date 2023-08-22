ec758f29087941dbb649ac57b894d9016cf04712
Marek Vana <marek810526@gmail.com>
2023-08-22 20:48:01 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,12 +68,11 @@ if uploaded_files:
         result = qa({"question": prompt, "chat_history": [(message["role"], message["content"]) for message in st.session_state.messages]})
 
         source_documents = result.get('source_documents', [])
-        sources = [doc['metadata']['source'] for doc in source_documents]
 
         with st.chat_message("assistant"):
             message_placeholder = st.empty()
             full_response = ""
-            full_response = result["answer"] + jsonpickle.encode(source_documents) + jsonpickle.encode(sources)
+            full_response = result["answer"] + jsonpickle.encode(source_documents)
             message_placeholder.markdown(full_response + "|")
         message_placeholder.markdown(full_response)    
         print(full_response)
