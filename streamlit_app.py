7cdfd9065b5b804fcdd76adbd5b9443604e14c95
marek_vana <marek810526@gmail.com>
2023-08-22 17:17:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,7 +70,7 @@ if uploaded_files:
         with st.chat_message("assistant"):
             message_placeholder = st.empty()
             full_response = ""
-            full_response = json.dumps(result)
+            full_response = ""
             message_placeholder.markdown(full_response + "|")
         message_placeholder.markdown(full_response)    
         print(full_response)
