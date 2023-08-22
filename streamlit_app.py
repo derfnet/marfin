ca707a607818f4a7cbe9b9df01eb8a2f4fe1d194
marek_vana <marek810526@gmail.com>
2023-08-22 18:48:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,10 +73,10 @@ if uploaded_files:
             message_placeholder = st.empty()
             full_response = ""
             full_response = result["answer"]
-            message_placeholder.markdown(full_response + "aaa")
+            message_placeholder.markdown(full_response + "|")
         message_placeholder.markdown(full_response)    
         print(full_response)
-        st.session_state.messages.append({"role": "assistant", "content": full_response})
+        st.session_state.messages.append({"role": "assistant", "content": type(result)})
 
 else:
     st.write("Prosím nahrajte soubory PDF.")
