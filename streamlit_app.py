27cddf7dae96ef43aaed754a6c3a5a8099744bfd
marek_vana <marek810526@gmail.com>
2024-05-27 08:57:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import os
 
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
-llm = ChatOpenAI(temperature=0, max_tokens=1000, model_name="gpt-3.5-turbo-16k")
+llm = ChatOpenAI(temperature=0, max_tokens=1000, model_name="gpt-4o")
 
 st.title("🤖Kognitivní vyhledávání v obsahu dokumetů.")
 
